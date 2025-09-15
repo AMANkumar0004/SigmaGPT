3b55b0a88139d1b34cdb78d4fb01662ae16e3e03
Rucha Ambaliya <Rambaliyaforall@gmail.com>
2025-09-15 15:58:25 +0530

    Add vercel.json

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -28,7 +28,7 @@ const ChatWindow = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:8080/api/chat", options);
+      const response = await fetch(`${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/chat`, options);
       const res = await response.json();
       console.log( res);
 
--- frontend/src/SideBar.jsx
+++ frontend/src/SideBar.jsx
@@ -10,7 +10,7 @@ const SideBar = () => {
 
       const getAllThreads = async ()=>{
       try {
-        const response =await fetch("http://localhost:8080/api/thread")
+        const response =await fetch(`${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/thread`)
         const res = await response.json();
         const filterData = res.map(thread=>({ threadId: thread.threadId,title: thread.title}))
         
@@ -37,7 +37,7 @@ const SideBar = () => {
       const changeThread =async(newThreadId)=>{
       setCurrThreadId(newThreadId);
       try{
-         const response = await  fetch(`http://localhost:8080/api/thread/${newThreadId}`)
+         const response = await  fetch(`${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/thread/${newThreadId}`)
          const res = await response.json();
          setPrevChats(Array.isArray(res) ? res : []);
          setNewChat(false);
